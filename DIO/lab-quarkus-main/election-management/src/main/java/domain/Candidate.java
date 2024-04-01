977c64ac5c6c83d2beacc85303820f04dfa9fee3
Marcos <marcsalexandr@gmail.com>
2024-04-01 00:51:40 -0300

    Desafio

--- DIO/lab-quarkus-main/election-management/src/main/java/domain/Candidate.java
+++ DIO/lab-quarkus-main/election-management/src/main/java/domain/Candidate.java
@@ -1,4 +1,21 @@
 package domain;
 
-public record Candidate() {
+import java.util.Optional;
+import java.util.UUID;
+
+public record Candidate(String id,
+                        Optional<String> photo,
+                        String givenName,
+                        String familyName,
+                        String email,
+                        Optional<String> phone,
+                        Optional<String> jobTitle) {
+    public static Candidate create(Optional<String> photo,
+                                   String givenName,
+                                   String familyName,
+                                   String email,
+                                   Optional<String> phone,
+                                   Optional<String> jobTitle) {
+        return new Candidate(UUID.randomUUID().toString(), photo, givenName, familyName, email, phone, jobTitle);
+    }
 }
